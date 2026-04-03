8ba62fda3f842b4264de7a609b80229ea02226fd
Edna <edna.guzogo@gmail.com>
2026-04-03 10:39:28 +0300

    auth middleware created

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+const protect = async (req, res, next) => {
+  let token = req.headers.authorization?.split(' ')[1] // Bearer <token>
+
+  if (!token) return res.status(401).json({ message: 'Not authorized' })
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    req.user = await User.findById(decoded.id).select('-password')
+    next()
+  } catch (err) {
+    res.status(401).json({ message: 'Token invalid' })
+  }
+}
+
+const adminOnly = (req, res, next) => {
+  if (req.user?.isAdmin) return next()
+  res.status(403).json({ message: 'Admin access only' })
+}
+
+module.exports = { protect, adminOnly }
