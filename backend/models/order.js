1d79e44f06aede0aa70255dc66124cf5b2c2da41
Edna <edna.guzogo@gmail.com>
2026-04-03 10:36:05 +0300

    backend models created

---
+++ backend/models/order.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const orderSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  items: [
+    {
+      product:  { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+      name:     String,
+      price:    Number,
+      quantity: Number,
+      image:    String,
+    }
+  ],
+  shippingAddress: {
+    address: String,
+    city:    String,
+    country: String,
+  },
+  totalPrice:  { type: Number, required: true },
+  isPaid:      { type: Boolean, default: false },
+  isDelivered: { type: Boolean, default: false },
+  paidAt:      Date,
+}, { timestamps: true })
+
+module.exports = mongoose.model('Order', orderSchema)
